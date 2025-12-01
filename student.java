0a370fa4fb5e88e3d85f6168d55ce37b2f1fba35
Manobhiramreddy <bmanobhiramreddy@gmail.com>
2025-12-01 07:26:58 +0530

    changes

--- student.java
+++ student.java
@@ -1,7 +1,7 @@
 package student;
 public class student {
   public static void main(String[] args) {
-    System.out.println("Hello from Pipeline CI!");
+    System.out.println("Hello Pipeline CI!");
     System.out.println("Hello!");
     System.out.println("Hello!");
   }
