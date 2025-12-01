672d28a9c1a7253793241225405857f9cd5d23b9
Manobhiramreddy <bmanobhiramreddy@gmail.com>
2025-12-01 07:22:27 +0530

    Initial Commit

---
+++ student.java
@@ -0,0 +1,8 @@
+package student;
+public class student {
+  public static void main(String[] args) {
+    System.out.println("Hello from Pipeline CI!");
+    System.out.println("Hello!");
+    System.out.println("Hello!");
+  }
+}
